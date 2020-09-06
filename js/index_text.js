88477893f88ccd273843f0c33bb280a9ced3967f
Dev Bhatia <32446285+dev-bhatia@users.noreply.github.com>
2020-09-06 11:31:11 -0700

    Good For Today

--- js/index_text.js
+++ js/index_text.js
@@ -1,6 +1,6 @@
 import { annotate } from "https://unpkg.com/rough-notation?module";
         const n1 = document.querySelector("#a1");
-        const a1 = annotate(n1, { type: "underline", color: "red" });
+        const a1 = annotate(n1, { type: "box", color: "gray" });
         var delayInMilliseconds = 500; // Time Delay in (ms)
 
         function hello(dispaly_element) {
